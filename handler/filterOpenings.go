68b1b53bfa65b0ab9b11e514f87a427a09e9b0d5
Pedro Passos <pedropassos.yt@gmail.com>
2024-11-19 20:56:57 -0500

    creating basic filter function

---
+++ handler/filterOpenings.go
@@ -0,0 +1,54 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/pedropassos06/gopportunities/schemas"
+)
+
+func ListFilteredOpeningsHandler(ctx *gin.Context) {
+	filters := make(map[string]interface{})
+
+	// read query params and add to filters
+	if role := ctx.Query("role"); role != "" {
+		filters["role"] = role
+	}
+	if location := ctx.Query("location"); location != "" {
+		filters["location"] = location
+	}
+	if remote := ctx.Query("remote"); remote != "" {
+		filters["remote"] = remote
+	}
+	if minSalary := ctx.Query("minSalary"); minSalary != "" {
+		filters["salary >"] = minSalary
+	}
+
+	// Call the generic filter function
+	openings, err := FilterOpenings(filters)
+	if err != nil {
+		sendError(ctx, http.StatusInternalServerError, "could not retrieve openings")
+		return
+	}
+
+	sendSuccess(ctx, "list-openings", openings)
+}
+
+func FilterOpenings(filters map[string]interface{}) ([]schemas.Opening, error) {
+	var openings []schemas.Opening
+
+	// start the query
+	query := db.Model(&schemas.Opening{})
+
+	// dynamically apply filters
+	for key, value := range filters {
+		query = query.Where(key+" = ?", value)
+	}
+
+	// execute the query
+	if err := query.Find(&openings).Error; err != nil {
+		return nil, err
+	}
+
+	return openings, nil
+}
