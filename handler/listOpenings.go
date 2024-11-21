d9c4cfd1047b99679dbbb490978b9da581fd9716
Pedro Passos <pedropassos.yt@gmail.com>
2024-11-20 22:14:12 -0500

    including filtering by company

--- handler/listOpenings.go
+++ handler/listOpenings.go
@@ -18,12 +18,60 @@ import (
 // @Failure 500 {object} ErrorResponse
 // @Router /openings [get]
 func ListOpeningsHandler(ctx *gin.Context) {
-	openings := []schemas.Opening{}
+	// Check if any query parameters are provided
+	filters := make(map[string]interface{})
+	if role := ctx.Query("role"); role != "" {
+		filters["role"] = role
+	}
+	if location := ctx.Query("location"); location != "" {
+		filters["location"] = location
+	}
+	if company := ctx.Query("company"); company != "" {
+		filters["company"] = company
+	}
+	if remote := ctx.Query("remote"); remote != "" {
+		filters["remote"] = remote
+	}
+	if minSalary := ctx.Query("minSalary"); minSalary != "" {
+		filters["salary >"] = minSalary
+	}
 
-	if err := db.Find(&openings).Error; err != nil {
-		sendError(ctx, http.StatusInternalServerError, "error listing openings")
+	// If no filters are provided, list all openings
+	if len(filters) == 0 {
+		var openings []schemas.Opening
+		if err := db.Find(&openings).Error; err != nil {
+			sendError(ctx, http.StatusInternalServerError, "could not retrieve openings")
+			return
+		}
+		sendSuccess(ctx, "list-openings", openings)
 		return
 	}
 
-	sendSuccess(ctx, "list-openings", openings)
+	// Otherwise, apply filters
+	openings, err := filterOpenings(filters)
+	if err != nil {
+		sendError(ctx, http.StatusInternalServerError, "could not retrieve openings")
+		return
+	}
+	sendSuccess(ctx, "list-filtered-openings", openings)
+}
+
+// filters openings based on a filters array
+func filterOpenings(filters map[string]interface{}) ([]schemas.Opening, error) {
+	var openings []schemas.Opening
+
+	// start the query
+	query := db.Model(&schemas.Opening{})
+
+	// dynamically apply filters
+	for key, value := range filters {
+		query = query.Where(key+" = ?", value)
+	}
+
+	// execute the query
+	if err := query.Find(&openings).Error; err != nil {
+		return nil, err
+	}
+
+	return openings, nil
 }
