6e3196c34b57bad5cd2bc38c475b0173df378e50
Pedro Passos <pedropassos.yt@gmail.com>
2024-11-22 23:31:37 -0500

    feat: create NewsletterSubscription entity

---
+++ schemas/newsletter_subscription.go
@@ -0,0 +1,24 @@
+package schemas
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type NewsletterSubscription struct {
+	gorm.Model
+	UserID     uint
+	Email      string
+	Subscribed bool
+}
+
+type NewsletterSubscriptionResponse struct {
+	ID         uint      `json:"id"`
+	CreatedAt  time.Time `json:"created_at"`
+	UpdatedAt  time.Time `json:"updated_at"`
+	DeletedAt  time.Time `json:"deleted_at,omitempty"`
+	UserID     uint      `json:"user_id"`
+	Email      string    `json:"email"`
+	Subscribed bool      `json:"subscribed"`
+}
