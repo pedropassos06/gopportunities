4503785f11429a0451fc4a6f970cf01d0384a863
Pedro Passos <pedropassos.yt@gmail.com>
2024-11-25 18:20:15 -0500

    feat: use environment variable for Google OAuth redirect URL

--- handler/auth_google.go
+++ handler/auth_google.go
@@ -15,7 +15,7 @@ func (h *Handler) GoogleAuthHandler(ctx *gin.Context) {
 	config := &oauth2.Config{
 		ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
 		ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
-		RedirectURL:  "http://localhost:8081/api/v1/auth/google/callback",
+		RedirectURL:  os.Getenv("GOOGLE_REDIRECT_URL"),
 		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email"},
 		Endpoint:     google.Endpoint,
 	}
