0466bc15e998ceda0a686ec693d36206fc4a0553
Pedro Passos <pedropassos.yt@gmail.com>
2024-11-26 22:16:55 -0500

    feat: implement JWT generation and validation helper functions

---
+++ helper/jwt.go
@@ -0,0 +1,46 @@
+package helper
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+var (
+	jwtSecret = []byte(os.Getenv("JWT_SECRET"))
+)
+
+func GenerateJWT(userInfo map[string]interface{}) (string, error) {
+	// Generate JWT token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": userInfo["id"],
+		"email":   userInfo["email"],
+		"exp":     time.Now().Add(24 * time.Hour).Unix(),
+	})
+
+	// sign the token with jwt secret
+	return token.SignedString(jwtSecret)
+}
+
+func ValidateToken(tokenString string) bool {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Validando o algoritmo de assinatura
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method")
+		}
+		return jwtSecret, nil
+	})
+
+	if err != nil {
+		return false
+	}
+
+	// Verifica se o token é válido e se as claims são confiáveis
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		exp := claims["exp"].(float64)
+		return time.Unix(int64(exp), 0).After(time.Now()) // Verifica expiração
+	}
+	return false
+}
