fce0bf396122c21db7a2857535a6283ee8b327de
Pedro Passos <pedropassos.yt@gmail.com>
2024-11-26 23:24:14 -0500

    include bearer token input in swagger doc

--- handler/createSubscription.go
+++ handler/createSubscription.go
@@ -14,6 +14,7 @@ import (
 // @Accept json
 // @Produce json
 // @Tags Newsletter
+// @Param Authorization header string true "Bearer token"
 // @Param request body NewsletterSubscriptionRequest true "Newsletter subscription details"
 // @Success 200 {object} NewsletterSubscriptionResponse
 // @Failure 400 {object} ErrorResponse
