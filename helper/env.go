0bdb1897cdaccc5bd6801ea8f62d1826da82bc13
Pedro Passos <pedropassos.yt@gmail.com>
2024-11-27 15:05:53 -0500

    feat: add helper functions for loading environment variables from .env file

---
+++ helper/env.go
@@ -0,0 +1,20 @@
+package helper
+
+// loads env file
+func LoadEnv() {
+	// Load .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+// loads env variables
+func LoadEnvVariable(key string) string {
+	// Load .env file
+	envVar := os.Getenv(key)
+	if envVar == "" {
+		fmt.Errorf("environment variable %s is not set", envVar)
+	}
+	return envVar
+}
