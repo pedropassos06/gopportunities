0fced7575dbff41e804ff7f1084e794cfc07f9a8
Pedro Passos <pedropassos.yt@gmail.com>
2024-11-27 22:15:53 -0500

    feat: create response bodies for Resume

--- resume/response.go
+++ resume/response.go
@@ -3,6 +3,16 @@ package resume
 import "github.com/pedropassos06/gopportunities/schemas"
 
 type UploadResumeResponse struct {
-	Message string                   `json:"message"`
-	Data    []schemas.ResumeResponse `json:"data"`
+	Message string                 `json:"message"`
+	Data    schemas.ResumeResponse `json:"data"`
+}
+
+type GetResumeResponse struct {
+	Message string                 `json:"message"`
+	Data    schemas.ResumeResponse `json:"data"`
+}
+
+type DeleteResumeResponse struct {
+	Message string                 `json:"message"`
+	Data    schemas.ResumeResponse `json:"data"`
 }
