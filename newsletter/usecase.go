f9dd751016486918ad7563ccd17d188b1d59dac9
Pedro Passos <pedropassos.yt@gmail.com>
2024-11-29 14:40:42 -0500

    feat: create usecase layer

---
+++ newsletter/usecase.go
@@ -0,0 +1,34 @@
+package newsletter
+
+import "github.com/pedropassos06/gopportunities/schemas"
+
+type NewsletterUsecase interface {
+	Subscribe(subscription schemas.NewsletterSubscription) error
+	Unsubscribe(subscription schemas.NewsletterSubscription) error
+	Find(email string) (schemas.NewsletterSubscription, error)
+}
+
+type NewsletterUsecaseImpl struct {
+	Repository NewsletterRepository
+}
+
+// NewNewsletterUsecase initializes and returns a NewsletterUsecase instance
+func NewNewsletterUsecase(repository NewsletterRepository) NewsletterUsecase {
+	return &NewsletterUsecaseImpl{
+		Repository: repository,
+	}
+}
+
+func (u *NewsletterUsecaseImpl) Subscribe(subscription schemas.NewsletterSubscription) error {
+	return u.Repository.Subscribe(subscription)
+}
+
+func (u *NewsletterUsecaseImpl) Unsubscribe(subscription schemas.NewsletterSubscription) error {
+	// set subscribed att to false
+	subscription.Subscribed = false
+	return u.Repository.Unsubscribe(subscription)
+}
+
+func (u *NewsletterUsecaseImpl) Find(email string) (schemas.NewsletterSubscription, error) {
+	return u.Repository.Find(email)
+}
