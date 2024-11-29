70b8f2609197f952f3a1067fc196519ac169bc46
Pedro Passos <pedropassos.yt@gmail.com>
2024-11-29 14:41:24 -0500

    feat: delete response boilerplate

--- schemas/newsletter_subscription.go
+++ schemas/newsletter_subscription.go
@@ -2,22 +2,13 @@ package schemas
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type NewsletterSubscription struct {
-	gorm.Model
-	UserID     uint
-	Email      string
-	Subscribed bool `gorm:"default:true"`
-}
-
-type NewsletterSubscriptionResponse struct {
 	ID         uint      `json:"id"`
 	CreatedAt  time.Time `json:"created_at"`
 	UpdatedAt  time.Time `json:"updated_at"`
 	UserID     uint      `json:"user_id"`
 	Email      string    `json:"email"`
-	Subscribed bool      `json:"subscribed"`
+	Subscribed bool      `json:"subscribed" gorm:"default:true"`
 }
