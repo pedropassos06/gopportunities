200ec0adc8acd164c7f23f2dcb79b49d46371e32
Pedro Passos <pedropassos.yt@gmail.com>
2024-11-29 17:19:36 -0500

    feat: use usecase for fetching resume and improve filename handling

--- resume/get_resume.go
+++ resume/get_resume.go
@@ -5,7 +5,6 @@ import (
 	"strings"
 
 	"github.com/gin-gonic/gin"
-	"github.com/pedropassos06/gopportunities/schemas"
 	"github.com/pedropassos06/gopportunities/utils"
 )
 
@@ -19,27 +18,29 @@ import (
 // @Produce multipart/form-data
 // @Param Authorization header string true "Bearer Token"
 // @Param user_id path string true "User ID of the resume owner"
-// @Success 200 {object} GetResumeResponse
+// @Success 200 {object} schemas.Resume
 // @Failure 400 {object} utils.ErrorResponse
 // @Failure 500 {object} utils.ErrorResponse
 // @Router /resume/{user_id} [get]
-func (rh *ResumeHandler) GetResumeHandler(ctx *gin.Context) {
+func (h *ResumeHandlerImpl) GetResumeHandler(ctx *gin.Context) {
 	// get user
 	userId := ctx.Param("user_id")
 	if userId == "" {
 		utils.SendError(ctx, http.StatusBadRequest, utils.ErrParamIsRequired("id", "path parameter").Error())
 	}
 
-	var resume schemas.Resume
 	// make sure it exists
-	if err := rh.DB.First(&resume, "user_id = ?", userId).Error; err != nil {
+	resume, err := h.Usecase.GetResumeByUserID(userId)
+	if err != nil {
 		utils.SendError(ctx, http.StatusNotFound, "resume not found")
 		return
 	}
 
 	// split path to get filename
 	parts := strings.Split(resume.Filepath, "/")
-	filename := parts[len(parts)-1]
+	filenameWithTimeStamp := parts[len(parts)-1]
+	// remove timestamp from filename (ex: 1732918410_resume.pdf)
+	filename := strings.Join(strings.Split(filenameWithTimeStamp, "_")[1:], "")
 
 	// return file as attachment
 	ctx.FileAttachment(resume.Filepath, filename)
