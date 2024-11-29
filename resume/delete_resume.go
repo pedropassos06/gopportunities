5f7161cfef81062c7ec48a5724e568e636d88fc3
Pedro Passos <pedropassos.yt@gmail.com>
2024-11-29 17:19:25 -0500

    feat: refactor DeleteResumeHandler to use usecase for resume operations

--- resume/delete_resume.go
+++ resume/delete_resume.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/pedropassos06/gopportunities/schemas"
 	"github.com/pedropassos06/gopportunities/utils"
 )
 
@@ -18,26 +17,28 @@ import (
 // @Produce json
 // @Param Authorization header string true "Bearer Token"
 // @Param resume_id path string true "ID of the resume"
-// @Success 200 {object} DeleteResumeResponse
+// @Success 200 {object} schemas.Resume
 // @Failure 400 {object} utils.ErrorResponse
 // @Failure 404 {object} utils.ErrorResponse
 // @Failure 500 {object} utils.ErrorResponse
 // @Router /resume/{resume_id} [delete]
-func (h *ResumeHandler) DeleteResumeHandler(ctx *gin.Context) {
+func (h *ResumeHandlerImpl) DeleteResumeHandler(ctx *gin.Context) {
 	// grab resume id
-	id := ctx.Param("resume_id")
-	if id == "" {
+	resumeID := ctx.Param("resume_id")
+	if resumeID == "" {
 		utils.SendError(ctx, http.StatusBadRequest, "resume id is required")
 		return
 	}
 
-	if err := h.DB.First(&schemas.Resume{}, id).Error; err != nil {
+	// make sure it exists
+	resume, err := h.Usecase.GetResumeByID(resumeID)
+	if err != nil {
 		utils.SendError(ctx, http.StatusNotFound, "resume not found")
 		return
 	}
 
 	// delete resume
-	if err := h.DB.Delete(&schemas.Resume{}, id).Error; err != nil {
+	if err := h.Usecase.DeleteResume(resume.ID); err != nil {
 		utils.SendError(ctx, http.StatusInternalServerError, "server is unable to delete resume at the moment")
 		return
 	}
