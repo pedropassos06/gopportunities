c79526e908a1593808d871dbefb7f4e08a8a2ec9
Pedro Passos <pedropassos.yt@gmail.com>
2024-12-04 21:46:05 -0500

    feat: enhance AuthHandler to include UserUseCase for improved user management

--- auth/handler.go
+++ auth/handler.go
@@ -1,14 +1,21 @@
 package auth
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/pedropassos06/gopportunities/user"
+)
 
 type AuthHandler interface {
 	GoogleAuthHandler(c *gin.Context)
 	GoogleCallbackHandler(c *gin.Context)
 }
 
-type AuthHandlerImpl struct{}
+type AuthHandlerImpl struct {
+	UserUseCase user.UserUsecase
+}
 
-func NewAuthHandler() AuthHandler {
-	return &AuthHandlerImpl{}
+func NewAuthHandler(usecase user.UserUsecase) AuthHandler {
+	return &AuthHandlerImpl{
+		UserUseCase: usecase,
+	}
 }
