e16fea10eb514dfd99292e9f03458c46f8727eb4
Pedro Passos <pedropassos.yt@gmail.com>
2024-12-04 21:13:52 -0500

    feat: add GetUserHandler to retrieve user by email

---
+++ user/get_user.go
@@ -0,0 +1,39 @@
+package user
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/pedropassos06/gopportunities/utils"
+)
+
+// @BasePath /api/v1
+
+// @Summary Get User
+// @Description Returns a user based on email
+// @Tags Users
+// @Accept json
+// @Produce json
+// @Param Authorization header string true "Bearer Token"
+// @Param id query string true "User email"
+// @Success 200 {object} schemas.User
+// @Failure 400 {object} utils.ErrorResponse "email parameter is missing"
+// @Failure 404 {object} utils.ErrorResponse "User not found"
+// @Router /user [get]
+func (h *UserHandlerImpl) GetUserHandler(ctx *gin.Context) {
+	// grab email parameter
+	email := ctx.Query("email")
+	if email == "" {
+		utils.SendError(ctx, http.StatusBadRequest, "email is required")
+		return
+	}
+
+	// find user
+	user, err := h.usecase.GetUser(email)
+	if err != nil {
+		utils.SendError(ctx, http.StatusNotFound, "user not found")
+		return
+	}
+
+	utils.SendSuccess(ctx, "get-user", user)
+}
